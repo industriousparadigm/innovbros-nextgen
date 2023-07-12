7d68847af815b31edf3e4c9324c32dc314380a04
theinnovationbrothers <121250245+theinnovationbrothers@users.noreply.github.com>
2023-07-12 12:42:16 +0100

    Update about.tsx

--- pages/about.tsx
+++ pages/about.tsx
@@ -7,11 +7,15 @@ export default function About() {
       <div className={styles.basicCenter}>
         <div className={styles.pageContent}>
           <p className={styles.description}>
-            We’re a team of innovators, passionate about what we do, on a mission to create groundbreaking concepts and improve your innovation output.
+            We’re a team of innovators, passionate about what we do,
+            on a mission to create groundbreaking concepts and improve
+            your innovation output.
           </p>
           <br />
           <p className={styles.description}>
-            With our ingenuity and systematic approach to creativity, and our complementary design and engineering experience, we ensure innovative solutions don’t happen by chance.
+            With our ingenuity and systematic approach to creativity,
+            and our complementary design and engineering experience,
+            we ensure innovative solutions don’t happen by chance.
           </p>
         </div>
       </div>
