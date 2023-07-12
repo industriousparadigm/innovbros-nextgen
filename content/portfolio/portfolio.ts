525fa53ac968c3254a0e32dea12d7f3e82ccd5c7
theinnovationbrothers <121250245+theinnovationbrothers@users.noreply.github.com>
2023-07-12 15:20:55 +0100

    Create portfolio.ts

---
+++ content/portfolio/portfolio.ts
@@ -0,0 +1,36 @@
+import { WorkItem } from "./types"
+
+export const workItems: WorkItem[] = [
+ {
+    title: "Money money",
+    author: "The Innovation Brothers",
+    coverImage: "",
+    description: "A collection of some projects we have worked over the years.",
+    content: [
+      {
+        type: "Paragraph",
+        text: "IPSUM ipsum and the whales and dolphins. IPSUM ipsum and the whales and dolphins. IPSUM ipsum and the whales and dolphins. IPSUM ipsum and the whales and dolphins. ",
+      },
+      {
+        type: "Paragraph",
+        text: "IPSUM ipsum and the whales and dolphins. And that's all folks.",
+      },
+    ],
+  },
+   {
+    title: "Money money",
+    author: "The Innovation Brothers",
+    coverImage: "",
+    description: "A collection of some projects we have worked over the years.",
+    content: [
+      {
+        type: "Paragraph",
+        text: "IPSUM ipsum and the whales and dolphins. IPSUM ipsum and the whales and dolphins. IPSUM ipsum and the whales and dolphins. IPSUM ipsum and the whales and dolphins. ",
+      },
+      {
+        type: "Paragraph",
+        text: "IPSUM ipsum and the whales and dolphins. And that's all folks.",
+      },
+    ],
+  },
+  ]
