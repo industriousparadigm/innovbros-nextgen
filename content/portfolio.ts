b1aeda30b8a2b1e1aad386bc006ab93b12955891
theinnovationbrothers <121250245+theinnovationbrothers@users.noreply.github.com>
2023-07-12 15:44:10 +0100

    Update portfolio.ts

--- content/portfolio.ts
+++ content/portfolio.ts
@@ -6,8 +6,8 @@ export const PortFolio: WorkItem[] = [
     author: "The Innovation Brothers",
     coverImage: "",
     description: "A collection of some projects we have worked over the years.",
-    content: [
-      {
+    content: export const workItems: WorkItem[] = [
+  {
     title: "Money money",
     author: "The Innovation Brothers",
     coverImage: "",
@@ -21,6 +21,49 @@ export const PortFolio: WorkItem[] = [
         type: "Paragraph",
         text: "IPSUM ipsum and the whales and dolphins. And that's all folks.",
       },
-    ]
-      ]
-      ]
+    ],
+  },
+  {
+    title: "Warm-up Energisers ebook",
+    author: "The Innovation Brothers",
+    coverImage: "senses-map.png",
+    description:
+      "This book brings together a collection of 52 handpicked Warm-up energisers’ tools and methods for better meetings and workshops.",
+    content: [
+      {
+        type: "Paragraph",
+        text: "This book brings together a collection of 52 handpicked Warm-up energisers’ tools and methods for better meetings and workshops.",
+      },
+      {
+        type: "Paragraph",
+        text: "This book brings together a collection of 52 handpicked Warm-up energisers’ tools and methods for better meetings and workshops.",
+      },
+      {
+        type: "Paragraph",
+        text: "This book brings together a collection of 52 handpicked Warm-up energisers’ tools and methods for better meetings and workshops.",
+      },
+      {
+        type: "Paragraph",
+        text: "And that's all folks!",
+      },
+    ],
+  },
+  {
+    title: "Machining Tool-sheet Calculator",
+    author: "The Innovation Brothers",
+    coverImage: "",
+    description:
+      "This WebApp is a work-in-progress Calculator that helps in preparing a Tool Sheet for programming and/or operating a Milling Machine.",
+    content: [
+      {
+        type: "Paragraph",
+        text: "Gonna do some dirty. ipsum and the whales and dolphins. IPSUM ipsum and the whales and dolphins. ",
+      },
+      {
+        type: "Paragraph",
+        text: "Save the pandas!",
+      },
+    ],
+  },
+]
+  ]
