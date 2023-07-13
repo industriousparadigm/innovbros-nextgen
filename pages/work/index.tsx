6360715f26bfb909baeb600008e1c90c763daf98
theinnovationbrothers <121250245+theinnovationbrothers@users.noreply.github.com>
2023-07-13 13:38:33 +0100

    Update index.tsx

--- pages/work/index.tsx
+++ pages/work/index.tsx
@@ -1,44 +1,56 @@
-import styles from "@/styles/Home.module.scss"
-import Layout from "@/components/layout"
-import { workItems } from "@/content/work"
-import Image from "next/image"
-import { Inter } from "@next/font/google"
-import Link from "next/link"
-import { slugify } from "@/utils/slugify"
+import styles from "@/styles/Home.module.scss";
+import Layout from "@/components/layout";
+import Image from "next/image";
+import Link from "next/link";
 
-export default function Work() {
+export default function HomePage() {
   return (
     <Layout>
       <div className={styles.basicCenter}>
         <div className={styles.pageContent}>
           <div className={styles.itemsGrid}>
-            {workItems.map((item) => (
-              <Link key={item.title} href={`/work/${slugify(item.title)}`}>
-                <div className={styles.gridItem}>
-                  <h3>{item.title}</h3>
-                  <div style={{ position: "relative", height: "200px" }}>
-                    <Image
-                      src={
-                        item.coverImage
-                          ? `/${item.coverImage}`
-                          : "/mystery-box.png"
-                      }
-                      alt="Foo"
-                      fill
-                      style={{ objectFit: "cover" }}
-                    />
-                  </div>
-                  <p>{item.description}</p>
-                  <br />
-                  <h5>
-                    Read more <span>-&gt;</span>
-                  </h5> 
-                </div>
-              </Link>
-            ))}
+            {/* First section */}
+            <Link href="../portfolio">
+            <a className={styles.gridItem}>
+              <h3>Portfolio</h3>
+              <div style={{ position: "relative", height: "200px" }}>
+                <Image
+                  src="/mystery-box.png"
+                  alt="Image 1"
+                  fill
+                  style={{ objectFit: "cover" }}
+                />
+              </div>
+              <p>A collection of our work</p>
+              <br />
+              <h5>
+                Read more <span>-&gt;</span>
+              </h5>
+            </a>
+            </Link>
+
+            {/* Second section */}
+            <Link href="../blog">
+            <a className={styles.gridItem}>
+              <h3>Blog</h3>
+              <div style={{ position: "relative", height: "200px" }}>
+                <Image
+                  src="/mystery-box.png"
+                  alt="Image 2"
+                  fill
+                  style={{ objectFit: "cover" }}
+                />
+              </div>
+              <p>Our writtings</p>
+              <br />
+              <h5>
+                Read more <span>-&gt;</span>
+              </h5>
+            </a>
+          </Link>
           </div>
         </div>
       </div>
     </Layout>
-  )
+  );
 }
