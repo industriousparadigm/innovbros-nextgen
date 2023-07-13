a99cd00c4abfdfe509bf402c3ee9ff484766dad4
theinnovationbrothers <121250245+theinnovationbrothers@users.noreply.github.com>
2023-07-13 13:28:48 +0100

    Update work.ts

--- content/work.ts
+++ content/work.ts
@@ -1,24 +1,6 @@
 import { WorkItem } from "./types"
-import { justCook } from "./blogposts/just-cook"
 
 export const workItems: WorkItem[] = [
-  justCook,
-  {
-    title: "Money money",
-    author: "The Innovation Brothers",
-    coverImage: "",
-    description: "A collection of some projects we have worked over the years.",
-    content: [
-      {
-        type: "Paragraph",
-        text: "IPSUM ipsum and the whales and dolphins. IPSUM ipsum and the whales and dolphins. IPSUM ipsum and the whales and dolphins. IPSUM ipsum and the whales and dolphins. ",
-      },
-      {
-        type: "Paragraph",
-        text: "IPSUM ipsum and the whales and dolphins. And that's all folks.",
-      },
-    ],
-  },
   {
     title: "Warm-up Energisers ebook",
     author: "The Innovation Brothers",
