124e0e46e6fad462797a32e9a1d530d8efce0e15
theinnovationbrothers <121250245+theinnovationbrothers@users.noreply.github.com>
2023-07-13 16:31:23 +0100

    Update work.ts

--- content/work.ts
+++ content/work.ts
@@ -1,6 +1,16 @@
 import { WorkItem } from "./types"
+import { Upport15 } from "./blogposts/upport-15"
+import { PerDon16 } from "./blogposts/per-don-16"
+import { OnFlick16 } from "./blogposts/on-flick-16"
+import { Lidless18 } from "./blogposts/lidless-18"
+import { DutchReach17 } from "./blogposts/dutch-reach-17"
 
 export const workItems: WorkItem[] = [
+  Upport15,
+  Lidless18,
+  DutchReach17,
+  OnFlick16,
+  PerDon16,
   {
     title: "Warm-up Energisers ebook",
     author: "The Innovation Brothers",
