e4e2e5bae0a1a95901ef9c881183ebad70b01703
theinnovationbrothers <121250245+theinnovationbrothers@users.noreply.github.com>
2023-07-14 16:08:00 +0100

    Update blog.ts

--- content/blog.ts
+++ content/blog.ts
@@ -27,21 +27,4 @@ export const workItems: WorkItem[] = [
   otherSenses,
   justCook,
   retailtainment,
-  
-  {
-    title: "Money money",
-    author: "The Innovation Brothers",
-    coverImage: "",
-    description: "A collection of some projects we have worked over the years.",
-    content: [
-      {
-        type: "Paragraph",
-        text: "IPSUM ipsum and the whales and dolphins. IPSUM ipsum and the whales and dolphins. IPSUM ipsum and the whales and dolphins. IPSUM ipsum and the whales and dolphins. ",
-      },
-      {
-        type: "Paragraph",
-        text: "IPSUM ipsum and the whales and dolphins. And that's all folks.",
-      },
-    ],
-  },
 ]
