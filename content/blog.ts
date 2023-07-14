64526bdb42ebb09011ca9b7215e99cc45f0f886f
theinnovationbrothers <121250245+theinnovationbrothers@users.noreply.github.com>
2023-07-14 16:06:05 +0100

    Update blog.ts

--- content/blog.ts
+++ content/blog.ts
@@ -1,8 +1,33 @@
 import { WorkItem } from "./types"
 import { justCook } from "./blogposts/just-cook"
+import { creativeChef } from "./blogposts/creative-chef"
+import { designMaps } from "./blogposts/design-maps"
+import { dooringLife } from "./blogposts/dooring-life"
+import { goldenMetrics } from "./blogposts/golden-innov-metrics"
+import { handlingThoughts } from "./blogposts/handling-thoughts"
+import { marsLatency } from "./blogposts/mars-latency"
+import { monotonousCars } from "./blogposts/monotonous-cars"
+import { otherSenses } from "./blogposts/other-senses"
+import { portableDesk } from "./blogposts/portable-desk"
+import { potatoesTelltale } from "./blogposts/potatoes-telltale"
+import { retailtainment } from "./blogposts/Retail-tainment"
+import { writingAI5Ways } from "./blogposts/writing-AI-5-ways"
 
 export const workItems: WorkItem[] = [
+  potatoesTelltale,
+  writingAI5Ways,
+  creativeChef,
+  portableDesk,
+  dooringLife,
+  designMaps,
+  goldenMetrics,
+  handlingThoughts,
+  marsLatency,
+  monotonousCars,
+  otherSenses,
   justCook,
+  retailtainment,
+  
   {
     title: "Money money",
     author: "The Innovation Brothers",
