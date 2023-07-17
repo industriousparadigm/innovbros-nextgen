a927620d5096cbea3f8eddd0e13a7e094e45614d
theinnovationbrothers <121250245+theinnovationbrothers@users.noreply.github.com>
2023-07-17 17:30:30 +0100

    Create warmup.ts

---
+++ content/toolkit/warmup.ts
@@ -0,0 +1,19 @@
+import { WorkItem } from "../types"
+import { Upport15 } from "../blogposts/upport-15"
+import { PerDon16 } from "../blogposts/per-don-16"
+import { OnFlick16 } from "../blogposts/on-flick-16"
+import { Lidless18 } from "../blogposts/lidless-18"
+import { DutchReach17 } from "../blogposts/dutch-reach-17"
+import { Booken15 } from "../blogposts/booken-15"
+import { SwollenPaper19 } from "../blogposts/swollen-paper-19"
+
+export const workItems: WorkItem[] = [
+  Upport15,
+  Lidless18,
+  DutchReach17,
+  OnFlick16,
+  PerDon16,
+  Booken15,
+  SwollenPaper19,
+
+]
