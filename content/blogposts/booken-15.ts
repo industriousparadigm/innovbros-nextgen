ec317d8e0b2403b5b5436430e9c5f99cc79159de
theinnovationbrothers <121250245+theinnovationbrothers@users.noreply.github.com>
2023-07-17 15:53:59 +0100

    Add files via upload

---
+++ content/blogposts/booken-15.ts
@@ -0,0 +1,56 @@
+import { WorkItem } from "../types"
+
+export const Booken: WorkItem = {
+  title: "Booken",
+  author: "The Innovation Brothers",
+  coverImage: "booken-15.png",
+  description:
+    "Booken (2015) physical e-book token to choose what to read.",
+  content: [
+    {
+      type: "Paragraph",
+      text: "Booken is a physical access token in the form of a book spine for e-readers. It enables people to show, collect and search for e-books to comfortably and conveniently read with their e-readers.",
+    },
+    {
+      type: "Paragraph",
+      text: "The challenge:",
+    },
+    {
+      type: "Paragraph",
+      text: "The distribution has been shifting from physical to digital formats. This enables users to easily access digital content over the Internet on any device, but raises a number of issues regarding its license, intangible format and piracy.",
+    },
+    {
+      type: "Paragraph",
+      text: "In the case of digital books, the book is never bought but leased from an intermediary that bulk purchases the copyrights from publishers and writers. Although this enables a more efficient distribution, with lower costs and to a larger audience, it leads to a lower willingness to pay and a less valued perception, as well as less money for creatives.",
+    },
+    {
+      type: "Paragraph",
+      text: "Besides these issues, there is the loss of the physical beauty and presence of books that adds value by making books interior design assets, collectible and nostalgic, while also difficulting the search for or the stumbling upon something to read.",
+    },
+    {
+      type: "Paragraph",
+      text: "The idea:",
+    },
+    {
+      type: "Paragraph",
+      text: "Booken solves these problems by giving a reader access to an e-book on any device with a physical access token, possessing the copyright license for the content, in the form of a book spine.",
+    },
+    {
+      type: "Image",
+      image: "booken-15-compare.png",
+      alt: "Booken combines books and e-books",
+    },
+    {
+      type: "Paragraph",
+      text: "Booken combines the physical qualities of books and the easiness and simplicity of reading on an e-reader. Like books, it is physical, it is tangible, easy to find, decorative or collectible (hence less disposable), resellable, and is cheap to manufacture and improves frontstore retail spaces. Like e-books, it is digital, it is easy to use, interactive and convenient.",
+    },
+    {
+      type: "Paragraph",
+      text: "Multiple options:",
+    },
+    {
+      type: "Paragraph",
+      text: "Most e-readers have an embedded store app to buy e-books. Steam sells video games' access codes as tokens. Specific sensitive databases and digital information are protected with cryptographic authentication.",
+    },
+  ],
+}
---
+++ content/blogposts/swollen-paper-19.ts
@@ -0,0 +1,55 @@
+import { WorkItem } from "../types"
+
+export const SwollenPaper: WorkItem = {
+  title: "Swollen Paper",
+  author: "The Innovation Brothers",
+  coverImage: "swollen-paper-19-cover.jpg",
+  description:
+    "Swollen Paper (2019) insulating recycled paper-based packaging .",
+  content: [
+    {
+      type: "Paragraph",
+      text: "A fully recyclable insulating paper-based packaging with a cheap and simple manufacturing process.",
+    },
+    {
+      type: "Paragraph",
+      text: "The challenge:",
+    },
+    {
+      type: "Paragraph",
+      text: "People are increasingly using eCommerce to purchase their day-to-day goods due to its convenience and ease. Some of the products bought are temperature sensitive, such as chocolate, that need to have their temperature controlled and stable during transport from warehouse to consumer.",
+    },
+    {
+      type: "Paragraph",
+      text: "Currently, temperature sensitive products are usually packed in a mixed material parcel with, for example, Expanded Polystyrene (EPS) or cool packs inside a cardboard box which are difficult or impossible to recycle. Additionally, consumers are strongly influenced and prepared to pay extra for sustainable solutions.",
+    },
+    {
+      type: "Paragraph",
+      text: "In this project we developed a paper-based thermal protection package for the eCommerce sales channel that is fully recyclable in the paper recycling system, and with similar or better insulation capabilities than current solutions.",
+    },
+    {
+      type: "Paragraph",
+      text: "The idea:",
+    },
+    {
+      type: "Paragraph",
+      text: "In 2019, we developed the Swollen Paper, a unique product made of a paper-based lightweight board that increases the insulating properties of paper by having a much lower weight per volume than other paper-based solutions.",
+    },
+    {
+      type: "Paragraph",
+      text: "The prototype had a density of less than 130kg/m3 (current paper products range from 250 to 1200 kg/m3), which can be improved with a refined manufacturing process and a different paper pulp (with recycled newspaper powder, water, and protein). The process used, expands the paper pulp with air, lowering its density and increasing its thermal protection.",
+    },
+    {
+      type: "Paragraph",
+      text: "The output material is fully recyclable in the paper recycling system.",
+    },
+    {
+      type: "Paragraph",
+      text: "Multiple options:",
+    },
+    {
+      type: "Paragraph",
+      text: "Besides the tradicional plastic (polystyrene foam), mycelium based packaging alternatives exist.",
+    },
+  ],
+}
