c8655c4a319cf5244eb9a4491cfba0dd1853f25b
theinnovationbrothers <121250245+theinnovationbrothers@users.noreply.github.com>
2023-07-17 15:59:08 +0100

    Update work.ts

--- content/work.ts
+++ content/work.ts
@@ -4,6 +4,8 @@ import { PerDon16 } from "./blogposts/per-don-16"
 import { OnFlick16 } from "./blogposts/on-flick-16"
 import { Lidless18 } from "./blogposts/lidless-18"
 import { DutchReach17 } from "./blogposts/dutch-reach-17"
+import { Booken15 } from "./blogposts/booken-15"
+import { SwollenPaper19 } from "./blogposts/swollen-paper-19"
 
 export const workItems: WorkItem[] = [
   Upport15,
@@ -11,47 +13,7 @@ export const workItems: WorkItem[] = [
   DutchReach17,
   OnFlick16,
   PerDon16,
-  {
-    title: "Warm-up Energisers ebook",
-    author: "The Innovation Brothers",
-    coverImage: "senses-map.png",
-    description:
-      "This book brings together a collection of 52 handpicked Warm-up energisers’ tools and methods for better meetings and workshops.",
-    content: [
-      {
-        type: "Paragraph",
-        text: "This book brings together a collection of 52 handpicked Warm-up energisers’ tools and methods for better meetings and workshops.",
-      },
-      {
-        type: "Paragraph",
-        text: "This book brings together a collection of 52 handpicked Warm-up energisers’ tools and methods for better meetings and workshops.",
-      },
-      {
-        type: "Paragraph",
-        text: "This book brings together a collection of 52 handpicked Warm-up energisers’ tools and methods for better meetings and workshops.",
-      },
-      {
-        type: "Paragraph",
-        text: "And that's all folks!",
-      },
-    ],
-  },
-  {
-    title: "Machining Tool-sheet Calculator",
-    author: "The Innovation Brothers",
-    coverImage: "",
-    description:
-      "This WebApp is a work-in-progress Calculator that helps in preparing a Tool Sheet for programming and/or operating a Milling Machine.",
-    content: [
-      {
-        type: "Paragraph",
-        text: "Gonna do some dirty. ipsum and the whales and dolphins. IPSUM ipsum and the whales and dolphins. ",
-      },
-      {
-        type: "Link",
-        text: "Save the pandas!",
-        url: "../about"
-      },
-    ],
-  },
+  Booken15,
+  SwollenPaper19,
+
 ]
