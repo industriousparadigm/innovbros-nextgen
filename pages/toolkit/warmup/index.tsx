d1645df9c42c6a4c69d71fcf508e9507f32bee3a
theinnovationbrothers <121250245+theinnovationbrothers@users.noreply.github.com>
2023-07-17 17:28:17 +0100

    Create index.tsx

---
+++ pages/toolkit/warmup/index.tsx
@@ -0,0 +1,44 @@
+import styles from "@/styles/Home.module.scss"
+import Layout from "@/components/layout"
+import { workItems } from "@/content/toolkit/warmup"
+import Image from "next/image"
+import { Inter } from "@next/font/google"
+import Link from "next/link"
+import { slugify } from "@/utils/slugify"
+
+export default function Work() {
+  return (
+    <Layout>
+      <div className={styles.basicCenter}>
+        <div className={styles.pageContent}>
+          <div className={styles.itemsGrid}>
+            {workItems.map((item) => (
+              <Link key={item.title} href={`/portfolio/${slugify(item.title)}`}>
+                <div className={styles.gridItem}>
+                  <h3 style={{ height: "50px" }}>{item.title}</h3>
+                  <div style={{ position: "relative", height: "200px" }}>
+                    <Image
+                      src={
+                        item.coverImage
+                          ? `/${item.coverImage}`
+                          : "/mystery-box.png"
+                      }
+                      alt="Foo"
+                      fill
+                      style={{ objectFit: "cover" }}
+                    />
+                  </div>
+                  <p>{item.description}</p>
+                  <br />
+                  <h5>
+                    Read more <span>-&gt;</span>
+                  </h5> 
+                </div>
+              </Link>
+            ))}
+          </div>
+        </div>
+      </div>
+    </Layout>
+  )
+}
