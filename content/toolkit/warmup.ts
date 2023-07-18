92a543d102a4fbe5107bd4d7c4f6ffe05fc08307
theinnovationbrothers <121250245+theinnovationbrothers@users.noreply.github.com>
2023-07-18 16:59:04 +0100

    Add files via upload

--- content/toolkit/warmup.ts
+++ content/toolkit/warmup.ts
@@ -5,6 +5,56 @@ import { ballToss } from "./warmup/02-ball-toss"
 import { birthline } from "./warmup/03-birthline"
 import { dreamVacation } from "./warmup/04-dream-vacation"
 import { favItem } from "./warmup/05-fav-item"
+import { fourFacts } from "./warmup/06-four-facts"
+import { haiku } from "./warmup/07-haiku"
+import { luckypenny } from "./warmup/08-lucky-penny"
+import { mapgame } from "./warmup/09-map-game"
+import { nickname } from "./warmup/10-nickname"
+import { nosyparkinson } from "./warmup/11-nosy-parkinson"
+import { peoplebingo } from "./warmup/12-people-bingo"
+import { provokingquestions } from "./warmup/13-provoking-questions"
+import { sketchneighbour } from "./warmup/14-sketch-neighbour"
+import { speeddating } from "./warmup/15-speed-dating"
+import { incommon } from "./warmup/16-in-common"
+import { toiletpaper } from "./warmup/17-toilet-paper"
+import { XXXcircles } from "./warmup/18-30-circles"
+import { adjacentpossible } from "./warmup/19-adjacent-possible"
+import { autowriting } from "./warmup/20-auto-writing"
+import { cartooncaption } from "./warmup/21-cartoon-caption"
+import { disposablepoetry } from "./warmup/22-disposable-poetry"
+import { emojificate } from "./warmup/23-emojificate"
+import { exquisitecorpse } from "./warmup/24-exquisite-corpse"
+import { oppositesattraction } from "./warmup/25-opposites-attraction"
+import { perfectwings } from "./warmup/26-perfect-wings"
+import { pointnshout } from "./warmup/27-pointnshout"
+import { rolladice } from "./warmup/28-rolladice"
+import { sundaydraw } from "./warmup/29-sunday-draw"
+import { thinkabout } from "./warmup/30-think-about"
+import { wingedbird } from "./warmup/31-winged-bird"
+import { wordassociation } from "./warmup/32-word-association"
+import { wordspinner } from "./warmup/33-word-spinner"
+import { coatarms } from "./warmup/34-coat-arms"
+import { criteriachairs } from "./warmup/35-criteria-chairs"
+import { doverb } from "./warmup/36-do-verb"
+import { drawflag } from "./warmup/37-draw-flag"
+import { GHOST } from "./warmup/38-GHOST"
+import { givegift } from "./warmup/39-give-gift"
+import { humanknot } from "./warmup/40-human-knot"
+import { mexicanwave } from "./warmup/41-mexican-wave"
+import { pantopitch } from "./warmup/42-panto-pitch"
+import { changeartist } from "./warmup/43-change-artist"
+import { brokenphone } from "./warmup/44-broken-phone"
+import { alphabet } from "./warmup/45-alphabet"
+import { echoball } from "./warmup/46-echo-ball"
+import { intensityball } from "./warmup/47-intensity-ball"
+import { kickoffclosing } from "./warmup/48-kickoff-closing"
+import { liearena } from "./warmup/49-lie-arena"
+import { phrasecaravan } from "./warmup/50-phrase-caravan"
+import { spellit } from "./warmup/51-spellit"
+import { threethings } from "./warmup/52-three-things"
+
+
+
 
 export const workItems: WorkItem[] = [
   warmupIntro,
@@ -13,4 +63,54 @@ export const workItems: WorkItem[] = [
   birthline,
   dreamVacation,
   favItem,
+  fourFacts,
+  haiku,
+  luckypenny,
+  mapgame,
+  nickname,
+  nosyparkinson,
+  peoplebingo,
+  provokingquestions,
+  sketchneighbour,
+  speeddating,
+  incommon,
+  toiletpaper,
+  XXXcircles,
+  adjacentpossible,
+  autowriting,
+  cartooncaption,
+  disposablepoetry,
+  emojificate,
+  exquisitecorpse,
+  oppositesattraction,
+  perfectwings,
+  pointnshout,
+  rolladice,
+  sundaydraw,
+  thinkabout,
+  wingedbird,
+  wordassociation,
+  wordspinner,
+  coatarms,
+  criteriachairs,
+  doverb,
+  drawflag,
+  GHOST,
+  givegift,
+  humanknot,
+  mexicanwave,
+  pantopitch,
+  changeartist,
+  brokenphone,
+  alphabet,
+  echoball,
+  intensityball,
+  kickoffclosing,
+  liearena,
+  phrasecaravan,
+  spellit,
+  threethings,
+  
+  
+  
 ]
