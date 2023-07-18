fd197f9cb52cab1ff1a0cd57e9b484cf14641c9c
theinnovationbrothers <121250245+theinnovationbrothers@users.noreply.github.com>
2023-07-18 12:58:58 +0100

    Create 01-artist-id.ts

---
+++ content/toolkit/warmup/01-artist-id.ts
@@ -0,0 +1,21 @@
+import { WorkItem } from "../types"
+
+export const 01artistID: WorkItem = {
+  title: "Artist ID",
+  author: "The Innovation Brothers",
+  coverImage: "01-artist-id-cover.png",
+  description:
+    "'Introduce Each Other' to promote people to get to know each other.",
+  content: [
+    {
+      type: "Image",
+      image: "01-artist-id.png",
+      alt: "artist ID",
+    },
+    {
+      type: "Link",
+      text: "Back to Warm-up Energisers",
+      url: "../toolkit/warmup",
+    },
+  ],
+}
