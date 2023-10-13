47437a2faf0916159301154e2f6c27118aee4b73
theinnovationbrothers <121250245+theinnovationbrothers@users.noreply.github.com>
2023-10-13 12:35:03 +0100

    Add files via upload

---
+++ content/blogposts/Voicefever-23.ts
@@ -0,0 +1,63 @@
+import { WorkItem } from "../types"
+
+export const Voicefever: WorkItem = {
+  title: "Voicefever UX Assessment",
+  author: "The Innovation Brothers",
+  coverImage: "voice-fever-23.png",
+  description:
+    "Voicefever UX Assessment (2023) a webapp for sending voice messages by email.",
+  content: [
+    {
+      type: "Paragraph",
+      text: "Voicefever is a webapp for sending voice messages by email, ensuring that the person who receives it will understand the context much better.",
+    },
+    {
+      type: "Paragraph",
+      text: "The challenge:",
+    },
+    {
+      type: "Paragraph",
+      text: "While the Voicefever webapp is technically sound and fully functional, there is room for improvement in terms of user experience. The current design and interaction flow could be refined to create a more streamlined and user-friendly interface.",
+    },
+    {
+      type: "Paragraph",
+      text: "The idea:",
+    },
+    {
+      type: "Paragraph",
+      text: "We conducted a UX assessment of the Voicefever mobile webapp and recommended a new screen flow design to enhance the user experience.",
+    },
+    {
+      type: "Image",
+      image: "voice-fever-UX-redesign-1.png",
+      alt: "page 1",
+    },
+    {
+      type: "Image",
+      image: "voice-fever-UX-redesign-2.png",
+      alt: "page 2",
+    },
+    {
+      type: "Image",
+      image: "voice-fever-UX-redesign-3.png",
+      alt: "page 3",
+    },
+    {
+      type: "Image",
+      image: "voice-fever-UX-redesign-4.png",
+      alt: "page 4",
+    },
+    {
+      type: "Paragraph",
+      text: "Multiple options:",
+    },
+    {
+      type: "Paragraph",
+      text: "Besides the current use case scenario of Voicefever: audio emails, our analysis showed that other potential use case scenarios included Annotation, Audio Newsletter, and Meeting Transcript.",
+    },
+    {
+      type: "Paragraph",
+      text: "Currently, a person can also send voice messages to another through a phone call, an instant messaging app such as WhatsApp or a VOIP service like Skype.",
+    },
+  ],
+}
