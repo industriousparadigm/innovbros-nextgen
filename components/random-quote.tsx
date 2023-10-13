53ae1e7701c16edbfad8caa32673fe765e03c20f
theinnovationbrothers <121250245+theinnovationbrothers@users.noreply.github.com>
2023-10-13 13:02:10 +0100

    Update random-quote.tsx

--- components/random-quote.tsx
+++ components/random-quote.tsx
@@ -32,7 +32,7 @@ const RandomQuote = () => {
             clipRule="evenodd"
           />
         </svg>
-        <p className={styles.centered}>Refresh for new quote</p>
+        <p className={styles.centered}>next quote</p>
       </div>
     </div>
   )
